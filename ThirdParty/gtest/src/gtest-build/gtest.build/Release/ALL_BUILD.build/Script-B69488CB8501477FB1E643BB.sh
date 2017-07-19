3f1116236ef0f32c86d8f9b7eba340c5a9587606
zero-impact <lawsonfulton@gmail.com>
2017-07-19 12:22:58 -0400

    Removing ThirdParty directory

--- ThirdParty/gtest/src/gtest-build/gtest.build/Release/ALL_BUILD.build/Script-B69488CB8501477FB1E643BB.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/dilevin/Documents/Research/Development/Gauss3/ThirdParty/gtest/src/gtest-build -f /Users/dilevin/Documents/Research/Development/Gauss3/ThirdParty/gtest/src/gtest-build/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all

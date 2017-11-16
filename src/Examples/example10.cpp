71a26ac5b86eda8e97d6f15bc1b25075a10ff942
David Levin <dilevin@csail.mit.edu>
2017-11-16 09:20:27 -0500

    Add example 10 for FEM-Spring coupling

---
+++ src/Examples/example10.cpp
@@ -0,0 +1,77 @@
+#include <functional>
+
+#include <Qt3DIncludes.h>
+#include <GaussIncludes.h>
+#include <FEMIncludes.h>
+
+//Any extra things I need such as constraints
+#include <ConstraintFixedPoint.h>
+#include <TimeStepperEulerImplicitLinear.h>
+
+using namespace Gauss;
+using namespace FEM;
+using namespace ParticleSystem; //For Force Spring
+
+/* Tetrahedral finite elements */
+
+//typedef physical entities I need
+
+//typedef scene
+typedef PhysicalSystemFEM<double, NeohookeanTet> FEMLinearTets;
+
+typedef World<double, std::tuple<FEMLinearTets *,PhysicalSystemParticleSingle<double> *>,
+std::tuple<ForceSpringFEMParticle<double> *>,
+std::tuple<ConstraintFixedPoint<double> *> > MyWorld;
+typedef TimeStepperEulerImplictLinear<double, AssemblerEigenSparseMatrix<double>,
+AssemblerEigenVector<double> > MyTimeStepper;
+
+typedef Scene<MyWorld, MyTimeStepper> MyScene;
+
+
+void preStepCallback(MyWorld &world) {
+    // This is an example callback
+}
+
+int main(int argc, char **argv) {
+    std::cout<<"Test Neohookean FEM \n";
+    
+    //Setup Physics
+    MyWorld world;
+    
+    //new code -- load tetgen files
+    Eigen::MatrixXd V;
+    Eigen::MatrixXi F;
+    
+    readTetgen(V, F, dataDir()+"/meshesTetgen/Beam/Beam.node", dataDir()+"/meshesTetgen/Beam/Beam.ele");
+    
+    FEMLinearTets *test = new FEMLinearTets(V,F);
+    PhysicalSystemParticleSingle<double> *test1 = new PhysicalSystemParticleSingle<double>();
+    test1->getImpl().setMass(100);
+    //ForceSpring<double> *forceSpring = new ForceSpring<double>(&test->getQ()[0], &test1->getQ(), 12.08, 2.0);
+    ForceSpringFEMParticle<double> *forceSpring = new ForceSpringFEMParticle<double>(PosFEM<double>(&test->getQ()[0],0, &test->getImpl().getV()),
+                                                                                     PosParticle<double>(&test1->getQ()),
+                                                                                     2, 100000.0);
+    
+    world.addSystem(test);
+    world.addSystem(test1);
+    world.addForce(forceSpring);
+    fixDisplacementMin(world, test);
+    world.finalize(); //After this all we're ready to go (clean up the interface a bit later)
+    
+    auto q = mapStateEigen(world);
+    q.setZero();
+    
+    auto q1 = mapDOFEigen(test1->getQ(), world);
+    q1[0] = -5.0;
+    
+    
+    MyTimeStepper stepper(0.01);
+    
+    //Display
+    QGuiApplication app(argc, argv);
+    
+    MyScene *scene = new MyScene(&world, &stepper, preStepCallback);
+    GAUSSVIEW(scene);
+    
+    return app.exec();
+}

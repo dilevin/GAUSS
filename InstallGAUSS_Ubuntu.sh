7c7554a62ec09ee95fc6eb7d1c2ee9729fdd66c9
David Levin <dilevin@csail.mit.edu>
2017-11-18 01:27:44 -0500

    a dd install scripts, not fully tested yet

---
+++ InstallGAUSS_Ubuntu.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+echo "Install GAUSS"
+echo "Download and Install Qt 5.9.0"
+wget http://download.qt.io/official_releases/qt/5.9/5.9.0/qt-opensource-linux-x64-5.9.0.run
+chmod +x qt-opensource-linux-x64-5.9.0.run
+./qt-opensource-linux-x64-5.9.0.run --platform minimal --script qt_script.qs
+
+echo "Install Additional Packages"
+sudo apt-get install build-essential
+sudo apt-get install xorg-dev
+sudo apt-get install cmake
+sudo apt-get install cmake-data
+sudo apt-get install libblas-dev
+sudo apt-get install liblapack-dev
+sudo apt-get install mesa-common-dev
+sudo apt-get install libglu1-mesa-dev -y
+sudo apt-get install libfontconfig1
+sudo apt-get install libeigen3-dev
+
+echo "Install libIGL"
+mkdir build
+cd build
+git clone --recursive https://github.com/libigl/libigl.git
+
+echo "Compile GAUSS"
+cmake .. -DCMAKE_PREFIX_PATH=~/Qt5.9.0/5.9/gcc_64/lib/cmake -DLIBIGL_INCLUDE_PATH=./libigl/include -DEigen3_DIR=/usr/include/eigen3
+make -j 2 all 
+                                                        

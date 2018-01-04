ec3d7bf8e391aece21fe115c371b206e6bb4fc79
vismay dgp lab <vismay@utexas.edu>
2018-01-04 18:01:33 -0500

    fixed plane strain area, and density update for mass matrix

--- src/Examples/example7.cpp
+++ src/Examples/example7.cpp
@@ -20,47 +20,47 @@ typedef Scene<MyWorld, MyTimeStepper> MyScene;
 
 int main(int argc, char **argv) {
     std::cout<<"Test neohookean material\n";
-    
+
     Eigen::setNbThreads(1);
-    
+
     std::cout<<"Test Linear FEM \n";
-    
+
     //Setup Physics
     MyWorld world;
-    
-    
+
+
     Eigen::MatrixXd V;
     Eigen::MatrixXi F;
-    
+
     //simple square subdivided into two triangles
     V.resize(4,3);
     V << 0,0,0,
          1,0,0,
          1,1,0,
          0,1,0;
-    
+
     F.resize(2,3);
     F << 0, 1, 2,
          0, 2, 3;
-    
+
     FEMPlaneStrainTri *test = new FEMPlaneStrainTri(V,F);
-    
+
     world.addSystem(test);
     fixDisplacementMin(world, test);
     world.finalize(); //After this all we're ready to go (clean up the interface a bit later)
-    
+
     auto q = mapStateEigen(world);
     q.setZero();
-    
+
     MyTimeStepper stepper(0.01);
-    
+
     stepper.step(world);
-    
+
     std::cout<<q<<"\n";
-    
-    
+
+
     return 1;
-    
-    
-    
+
+
+
 }

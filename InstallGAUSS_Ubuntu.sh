cc1a6aa2a906c1a495637f1313525eeebdb55da7
David I.W. Levin <dilevin@csail.mit.edu>
2018-04-13 18:14:35 -0400

    Update InstallGAUSS_Ubuntu.sh

--- InstallGAUSS_Ubuntu.sh
+++ InstallGAUSS_Ubuntu.sh
@@ -21,6 +21,6 @@ mkdir build
 cd build
 
 echo "Compile GAUSS"
-cmake .. -DCMAKE_PREFIX_PATH=/opt/Qt5.9.0/5.9/gcc_64/lib/cmake -DLIBIGL_INCLUDE_PATH=./libigl/include -DEigen3_DIR=/usr/include/eigen3 -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_PREFIX_PATH=/opt/Qt5.9.0/5.9/gcc_64/lib/cmake -DEigen3_DIR=/usr/include/eigen3 -DCMAKE_BUILD_TYPE=Release
 make -j 2 all 
                                                         

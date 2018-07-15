f8f87bf60cdd7afd92387d18120350c70252e211
David Levin <dilevin@csail.mit.edu>
2018-07-15 17:12:59 -0400

    Make linear energy subclass virtual shape function, add implicit integration example

---
+++ src/Optimization/include/NewtonReducedHessian.h
@@ -0,0 +1,12 @@
+//
+//  NewtonReducedHessian.h
+//  Gauss
+//
+//  Created by David Levin on 6/25/18.
+//
+
+#ifndef NewtonReducedHessian_h
+#define NewtonReducedHessian_h
+
+
+#endif /* NewtonReducedHessian_h */
